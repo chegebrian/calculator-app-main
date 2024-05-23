9cd8355b364f52ae73bae2300ce32e052651943f
Brian Waithaka <waithakabrian8@gmail.com>
2024-05-23 20:28:06 +0300

    implement calculate functionality

--- script.js
+++ script.js
@@ -19,3 +19,64 @@ const pointEl = document.querySelector(".point");
 const multEl = document.querySelector(".mult");
 const resetEl = document.querySelector(".reset");
 const submitEl = document.querySelector(".submit");
+
+
+oneEl.addEventListener("click",() => {
+    outputEl.value += oneEl.value;
+});
+twoEl.addEventListener("click",() => {
+    outputEl.value += twoEl.value;
+});
+threeEl.addEventListener("click",() => {
+    outputEl.value += threeEl.value;
+});
+fourEl.addEventListener("click",() => {
+    outputEl.value += fourEl.value;
+});
+fiveEl.addEventListener("click",() => {
+    outputEl.value += fiveEl.value;
+});
+sixEl.addEventListener("click",() => {
+    outputEl.value += sixEl.value;
+});
+sevenEl.addEventListener("click",() => {
+    outputEl.value += sevenEl.value;
+});
+eightEl.addEventListener("click",() => {
+    outputEl.value += eightEl.value;
+});
+nineEl.addEventListener("click",() => {
+    outputEl.value += nineEl.value;
+});
+zeroEl.addEventListener("click",() => {
+    outputEl.value += zeroEl.value;
+});
+addEl.addEventListener("click",() => {
+    outputEl.value += addEl.value;
+});
+multEl.addEventListener("click",() => {
+    outputEl.value += multEl.value;
+});
+divEl.addEventListener("click",() => {
+    outputEl.value += divEl.value;
+});
+subEl.addEventListener("click",() => {
+    outputEl.value += subEl.value;
+});
+pointEl.addEventListener("click",() => {
+    outputEl.value += pointEl.value;
+});
+resetEl.addEventListener("click",() => {
+    outputEl.value = "";
+});
+delEl.addEventListener("click",() => {
+    outputEl.value = outputEl.value.slice(0, -1);
+});
+submitEl.addEventListener("click",() => {
+    try {
+        outputEl.value = eval?.(outputEl.value)
+        
+    } catch (error) {
+        outputEl.value = "Error"
+    }
+});
