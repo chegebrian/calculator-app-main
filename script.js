ea238d66a887133ed6c44ec3fe3858fff75ec07d
Brian Waithaka <waithakabrian8@gmail.com>
2024-05-23 05:23:38 +0300

    select elements from the dom

---
+++ script.js
@@ -0,0 +1,21 @@
+const outputEl = document.querySelector(".output");
+
+// button elements
+const oneEl = document.querySelector(".one");
+const twoEl = document.querySelector(".two");
+const threeEl = document.querySelector(".three");
+const fourEl = document.querySelector(".four");
+const fiveEl = document.querySelector(".five");
+const sixEl = document.querySelector(".six");
+const sevenEl = document.querySelector(".seven");
+const eightEl = document.querySelector(".eight");
+const nineEl = document.querySelector(".nine");
+const zeroEl = document.querySelector(".zero");
+const delEl = document.querySelector(".del");
+const addEl = document.querySelector(".add");
+const subEl = document.querySelector(".sub");
+const divEl = document.querySelector(".div");
+const pointEl = document.querySelector(".point");
+const multEl = document.querySelector(".mult");
+const resetEl = document.querySelector(".reset");
+const submitEl = document.querySelector(".submit");
